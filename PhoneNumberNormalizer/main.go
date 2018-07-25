ecfc922030286ee16dd3e3df256bb2680d9f477d
Pengcheng Liu <pengcheng.liu@recaresolutions.com>
2018-07-26 00:31:50 +0200

    add psql db

--- PhoneNumberNormalizer/main.go
+++ PhoneNumberNormalizer/main.go
@@ -2,10 +2,40 @@ package main
 
 import (
 	"bytes"
+	"database/sql"
+	"fmt"
 	"regexp"
+
+	_ "github.com/lib/pq"
+)
+
+const (
+	host     = "localhost"
+	port     = 5432
+	user     = "pengchengliu"
+	password = "thepassword"
+	dbname   = "gophercise_phone"
 )
 
 func main() {
+	psqlInfo := fmt.Sprintf("host=%s port=%d user=%s password=%s sslmode=disable", host, port, user, password)
+	db, err := sql.Open("postgres", psqlInfo)
+	if err != nil {
+		panic(err)
+	}
+	err = createDB(db, dbname)
+	if err != nil {
+		panic(err)
+	}
+	db.Close()
+}
+
+func createDB(db *sql.DB, dbname string) error {
+	_, err := db.Exec("CREATE DATABASE " + dbname)
+	if err != nil {
+		return err
+	}
+	return nil
 }
 
 func normalize(phone string) string {
