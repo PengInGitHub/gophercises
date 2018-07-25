9e8d615e7d3dd4cd37c36e7e3b5aed459ea57c3f
Pengcheng Liu <pengcheng.liu@recaresolutions.com>
2018-07-25 08:35:24 +0200

    add regexp function and test

--- PhoneNumberNormalizer/main.go
+++ PhoneNumberNormalizer/main.go
@@ -1,8 +1,24 @@
 package main
 
+import (
+	"bytes"
+	"regexp"
+)
+
 func main() {
 }
 
 func normalize(phone string) string {
-	return ""
+	var buf bytes.Buffer
+	for _, ch := range phone {
+		if ch <= '9' && ch >= '0' {
+			buf.WriteRune(ch)
+		}
+	}
+	return buf.String()
+}
+
+func normalizeREGEXP(phone string) string {
+	re := regexp.MustCompile("\\D") //find all non int
+	return re.ReplaceAllString(phone, "")
 }
