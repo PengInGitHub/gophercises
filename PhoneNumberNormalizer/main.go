a58c6f720a7bf102d8ba38372e0f144fe5a050d1
Pengcheng Liu <pengcheng.liu@recaresolutions.com>
2018-07-27 03:41:03 +0200

    query records

--- PhoneNumberNormalizer/main.go
+++ PhoneNumberNormalizer/main.go
@@ -19,27 +19,58 @@ const (
 
 func main() {
 	psqlInfo := fmt.Sprintf("host=%s port=%d user=%s password=%s sslmode=disable", host, port, user, password)
-	db, err := sql.Open("postgres", psqlInfo)
-	handleErr(err)
+	// db, err := sql.Open("postgres", psqlInfo)
+	// handleErr(err)
 
-	//create DB
-	err = resetDB(db, dbname)
-	handleErr(err)
-	db.Close()
+	// //create DB
+	// err = resetDB(db, dbname)
+	// handleErr(err)
+	// db.Close()
 
 	//disconnect DB, create tables
 	psqlInfo = fmt.Sprintf("%s dbname=%s", psqlInfo, dbname)
-	db, err = sql.Open("postgres", psqlInfo)
+	db, err := sql.Open("postgres", psqlInfo)
 	handleErr(err)
 	defer db.Close()
-	//ping the DB
-	handleErr(db.Ping()) //ping: test the connectivity
+	handleErr(createPhoneNumbersTable(db))
+	_, err = insertPhoneNumber(db, "1234567890")
+	handleErr(err)
+	_, err = insertPhoneNumber(db, "123 456 7891")
+	handleErr(err)
+	_, err = insertPhoneNumber(db, "(123) 456 7892")
+	handleErr(err)
+	_, err = insertPhoneNumber(db, "(123) 456-7893")
+	handleErr(err)
+	_, err = insertPhoneNumber(db, "123-456-7890")
+	handleErr(err)
+	_, err = insertPhoneNumber(db, "(123)456-7892")
+	handleErr(err)
 }
 
 func handleErr(err error) {
 	if err != nil {
 		panic(err)
 	}
+}
+
+func insertPhoneNumber(db *sql.DB, phone string) (int, error) {
+	statement := `INSERT INTO phone_numbers(value) VALUES($1) RETURNING id`
+	var id int
+	err := db.QueryRow(statement, phone).Scan(&id)
+	if err != nil {
+		return -1, err
+	}
+	return id, nil
+}
+
+func createPhoneNumbersTable(db *sql.DB) error {
+	statement := `
+    CREATE TABLE IF NOT EXISTS phone_numbers (
+      id SERIAL,
+      value VARCHAR(255)
+    )`
+	_, err := db.Exec(statement)
+	return err
 }
 
 func createDB(db *sql.DB, dbname string) error {
